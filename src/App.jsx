7db328e58a562b24efef38f3dbbb1c34e1072498
ElinFoUni <3ggapp24.tvinel@folkuniversitetet.nu>
2025-10-09 21:33:50 +0200

    Added the route to the App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 import MoviesPage from "./pages/MoviesPage"
 import MovieDetailPage from "./pages/MovieDetailPage"
 import CartPage from './pages/CartPage';
+import SearchPage from "./pages/SearchPage"
 
 function App() {
   return (
@@ -11,7 +12,8 @@ function App() {
        <Routes>
         <Route path="/" element={<MoviesPage/>} />
         <Route path="cart" element={<CartPage/>}/>
-        <Route path="/movie/:id" element={<MovieDetailPage/>} />
+        <Route path="/movie/:id" element={<MovieDetailPage/>}/>
+        <Route path="/search" element={<SearchPage/>}/>
        </Routes>
       </div>
     </>

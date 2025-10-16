144ad77098525989787df847737ce91a3e650c3c
malmz90 <70096644+malmz90@users.noreply.github.com>
2025-10-16 10:20:44 +0200

    better back button

--- src/pages/CartPage.jsx
+++ src/pages/CartPage.jsx
@@ -2,6 +2,7 @@ import { useNavigate } from "react-router-dom";
 import Header from "../components/Header";
 import CartList from "../components/CartList";
 import CheckoutForm from "../components/CheckoutForm";
+import backIcon from "../assets/backicone.png";
 import "../style/CartPage.css";
 
 export default function CartPage() {
@@ -14,9 +15,12 @@ export default function CartPage() {
   return (
     <div className="cart-page">
       <Header />
-      <button className="back-button" onClick={handleBackClick}>
-        ←
-      </button>
+      <img 
+        src={backIcon} 
+        alt="Back" 
+        className="back-icon-clickable"
+        onClick={handleBackClick}
+      />
       <div className="cart-layout">
         <CartList />
        <CheckoutForm/>
